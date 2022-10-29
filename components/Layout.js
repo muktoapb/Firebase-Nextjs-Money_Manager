2a4fa174d2d7e4ac9e913b3fa98cfdfe9e650626
Mukto <muktoapb@gmail.com>
2022-10-29 12:00:38 +0600

    About page added

--- components/Layout.js
+++ components/Layout.js
@@ -10,23 +10,24 @@ export default function Layout({ children }) {
   const [user, loading, error] = useAuthState(auth);
   return (
     <>
-    
+
       {
         user ?
           <div className='min-h-screen bg-slate-50 h-full font-SourceSansPro'>
-          <Header />
-          <div className="flex font-Poppins min-h-screen">
-            <div className="w-80 py-8 min-h-screen">
-            <Sidebar/>
-            </div>
-            <div className="w-full">
-              <div className="py-8 px-8">
-              {children}
+            <Header />
+            <div className="flex font-Poppins min-h-screen">
+              <div className="w-80 py-8 min-h-screen">
+                <Sidebar />
+              </div>
+              <div className="w-full">
+                <div className="py-8 px-8">
+                  {children}
+                </div>
               </div>
             </div>
+            
           </div>
-          </div>
-          
+
           :
           <Login />
       }
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import React from 'react'
-import Breadcam from '../components/Breadcam'
-
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import mukto from '../assets/img/mukto.png';
+import Breadcam from '../components/Breadcam';
 export default function About() {
     return (
         <>
@@ -11,7 +12,23 @@ export default function About() {
 
 
             <Breadcam title='About' />
-            Mahidul islam Mukto
+            <div className="">
+                <h4 className='text-lg font-semibold mb-2'>About Developer</h4>
+                <div className="flex gap-4">
+                    <div className="max-w-[200px]">
+                        <Image src={mukto} alt="Mahidul Islam Mukto"></Image>
+                    </div>
+                    <div className="text-slate-600">
+                        <p>Mahidul Islam Mukto is a Frontend webdeveloper and WordPress Expart.Mukto is professional web development for more than half-decade. Besides Web Development he love to Travel, Design website UI, and gather knowledge in new technology. </p>
+                    </div>
+                </div>
+                <h4 className='text-lg font-semibold my-2'>About This APP</h4>
+                <div className="text-slate-600 tracking-wide">
+                        <p>App is build for a special parpase. Money Management is very import for our life to avoid bad times. As Muslim I belive Allah (God) give us eveything. We don't earn anything from ourself. In every business partner is importent. Business profite or loss also depend on your business partner. In my case I keep Allah as my business partner. I give him 2.5% partnership. That's means 2.5% of my earning I give charity or poor people who badly need money. If you from other religion you can think it as just donate. This App is design for All of the people who want to donate a % of their income. </p>
+                        <br/>
+                        <p>There is another part of the APP is Investment. Investment is very importent for growing up. I keep 5% of my earning for Investment. This App will automatically keep 5% money for investment and you can spand it for new business or your existing business growing.</p>
+                    </div>
+            </div>
         </>
     )
 }
