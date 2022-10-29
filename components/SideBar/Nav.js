67b4d75e2e469777ff60ad99eefb52b1fe753288
Mukto <muktoapb@gmail.com>
2022-10-29 21:41:26 +0600

    Nav fixed on desktop

--- components/SideBar/Nav.js
+++ components/SideBar/Nav.js
@@ -9,29 +9,29 @@ export default function Nav() {
     <nav className='w-full'>
       <ul className='text-lg text-slate-600 font-medium md:block flex w-full justify-between'>
 
-        <li className={router.pathname=='/' ? 'navitem_design border-2 border-sky-500': 'hover:navitem_design border-2 border-transparent hover:border-sky-500'}>
+        <li className={router.pathname=='/' ? 'navitem_design border-2 md:border-none border-sky-500': 'hover:navitem_design border-2 md:border-none border-transparent hover:border-sky-500'}>
           <Link href='/'> 
            <a className='navitem_defult'><MdOutlineDashboard className='md:mr-1'/> <span className="hidden md:block">Dashboard</span></a>
           </Link>
         </li>
 
-        <li className={router.pathname=='/earning' ? 'navitem_design border-2 border-sky-500': 'hover:navitem_design border-2 border-transparent hover:border-sky-500'}>
+        <li className={router.pathname=='/earning' ? 'navitem_design border-2 md:border-none border-sky-500': 'hover:navitem_design border-2 md:border-none border-transparent hover:border-sky-500'}>
           <Link href='/earning'><a className="navitem_defult"><MdOutlineAccountBalanceWallet className='md:mr-1'/><span className="hidden md:block">Earning</span> </a></Link>
         </li>
 
-        <li className={router.pathname=='/donate' ? 'navitem_design border-2 border-sky-500': 'hover:navitem_design border-2 border-transparent hover:border-sky-500'}>
+        <li className={router.pathname=='/donate' ? 'navitem_design border-2 md:border-none border-sky-500': 'hover:navitem_design border-2 md:border-none border-transparent hover:border-sky-500'}>
           <Link href='/donate'><a className="navitem_defult"><BiDonateHeart className='md:mr-1'/><span className="hidden md:block">Donate</span> </a></Link>
         </li>
         
-        <li className={router.pathname=='/investment' ? 'navitem_design border-2 border-sky-500': 'hover:navitem_design border-2 border-transparent hover:border-sky-500'}>
+        <li className={router.pathname=='/investment' ? 'navitem_design border-2 md:border-none border-sky-500': 'hover:navitem_design border-2 md:border-none border-transparent hover:border-sky-500'}>
           <Link href='/investment'><a className="navitem_defult"><IoBriefcaseOutline className='md:mr-1'/><span className="hidden md:block">Investment</span> </a></Link>
         </li>
         
-        <li className={router.pathname=='/expense' ? 'navitem_design border-2 border-sky-500': 'hover:navitem_design border-2 border-transparent hover:border-sky-500'}>
+        <li className={router.pathname=='/expense' ? 'navitem_design border-2 md:border-none border-sky-500': 'hover:navitem_design border-2 md:border-none border-transparent hover:border-sky-500'}>
           <Link href='/expense'><a className="navitem_defult"><MdPayment className='md:mr-1'/><span className="hidden md:block">Expense</span> </a></Link>
         </li>
 
-        <li className={router.pathname=='/about' ? 'navitem_design border-2 border-sky-500': 'hover:navitem_design border-2 border-transparent hover:border-sky-500'}>
+        <li className={router.pathname=='/about' ? 'navitem_design border-2 md:border-none border-sky-500': 'hover:navitem_design border-2 md:border-none border-transparent hover:border-sky-500'}>
           <Link href='/about'><a className="navitem_defult"><MdInfoOutline className='md:mr-1'/><span className="hidden md:block">About</span> </a></Link>
         </li>
       </ul>
