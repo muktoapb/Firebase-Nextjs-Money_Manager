806fe20f883e13545efd7de6ad1230d76b6430eb
Mukto <muktoapb@gmail.com>
2022-11-14 18:43:41 +0600

    pagination next prev issue fixed

--- components/Money/MoneyList.js
+++ components/Money/MoneyList.js
@@ -4,54 +4,52 @@ import MoneyItem from './MoneyItem';
 import Pagination from './Pagination';
 let PageSize = 5;
 
-export default function MoneyList({ money, title, db_name,earning }) {
+export default function MoneyList({ money, title, db_name, earning }) {
     const moneyList = money;
-    console.log(moneyList);
     const [currentPage, setCurrentPage] = useState(1);
 
-  const currentTableData = useMemo(() => {
-    const firstPageIndex = (currentPage - 1) * PageSize;
-    const lastPageIndex = firstPageIndex + PageSize;
-    console.log();
-    return moneyList.slice(firstPageIndex, lastPageIndex);
-  }, [currentPage,moneyList]);
+    const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return moneyList.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage, moneyList]);
 
-    let totalMoney = 0;
-console.log(currentTableData);
+    let totalMoney = moneyList.reduce(function (prev, cur) {
+        return prev + cur.amount;
+    }, 0);
+    
     return (
-        moneyList.length>0&&(
+        moneyList.length > 0 && (
             <div className='border-dashed border-2 py-3 px-3 rounded-lg'>
-            <h3 className='font-semibold'>{title}</h3>
-            <div className="mt-2">
-                {
-                    currentTableData.map((data) => {
-                        let amount = 0;
-                        // console.log(data);
-                        if (earning) {
-                            amount = data.expense
-                        }else{
-                            amount = data.amount
-                        }
-                        totalMoney = totalMoney + amount;
+                <h3 className='font-semibold'>{title}</h3>
+                <div className="mt-2">
+                    {
+                        currentTableData.map((data) => {
+                            let amount = 0;
+                            // console.log(data);
+                            if (earning) {
+                                amount = data.expense
+                            } else {
+                                amount = data.amount
+                            }
 
-                        return <MoneyItem date={data.date} amount={amount} text={data.comment} itemid={data.id} key={data.id} db_name={db_name} ></MoneyItem>;
-                    })
-                }
-            </div>
-            <div className="mt-3 border-t-2 border-dashed pt-2 tracking-wider">
-            <Pagination
-                currentPage={currentPage}
-                totalCount={moneyList.length}
-                pageSize={PageSize}
-                onPageChange={page => setCurrentPage(page)}
-            />
-            </div>
-            <div className="mt-3 border-t-2 border-dashed pt-2 tracking-wider">
-                <p>Total : {numberFormater(totalMoney)}</p>
-            </div>
+                            return <MoneyItem date={data.date} amount={amount} text={data.comment} itemid={data.id} key={data.id} db_name={db_name} ></MoneyItem>;
+                        })
+                    }
+                </div>
+                <Pagination
+                        currentPage={currentPage}
+                        totalCount={moneyList.length}
+                        pageSize={PageSize}
+                        onPageChange={page => setCurrentPage(page)}
+                    />
+                
+                <div className="mt-3 border-t-2 border-dashed pt-2 tracking-wider">
+                    <p>Total : {numberFormater(totalMoney)}</p>
+                </div>
 
 
-        </div>
+            </div>
         )
     )
 }
--- components/Money/Pagination.js
+++ components/Money/Pagination.js
@@ -2,63 +2,72 @@ import React from 'react';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import { DOTS, usePagination } from '../../utils/usePagination';
 const Pagination = props => {
-  const {
-    onPageChange,
-    totalCount,
-    siblingCount = 1,
-    currentPage,
-    pageSize,
-  } = props;
+    const {
+        onPageChange,
+        totalCount,
+        siblingCount = 1,
+        currentPage,
+        pageSize,
+    } = props;
 
-  const paginationRange = usePagination({
-    currentPage,
-    totalCount,
-    siblingCount,
-    pageSize
-  });
+    const paginationRange = usePagination({
+        currentPage,
+        totalCount,
+        siblingCount,
+        pageSize
+    });
 
-  if (currentPage === 0 || paginationRange.length < 2) {
-    return null;
-  }
+    if (currentPage === 0 || paginationRange.length < 2) {
+        return null;
+    }
 
-  const onNext = () => {
-    onPageChange(currentPage + 1);
-  };
-
-  const onPrevious = () => {
-    onPageChange(currentPage - 1);
-  };
-
-  let lastPage = paginationRange[paginationRange.length - 1];
-  return (
-    <ul className="flex items-center gap-1">
-      <li
-        className={`bg-slate-200 w-6 h-6 text-sm leading-6 rounded-full text-center flex justify-center items-center ${currentPage === 1?'opacity-25 cursor-not-allowed':'cursor-pointer'}`}
-        onClick={onPrevious}
-      >
-        <BsChevronLeft/>
-      </li>
-      {paginationRange.map((pageNumber,index) => {
-        if (pageNumber === DOTS) {
-          return <li className="bg-slate-200 w-6 h-6 text-sm leading-6 rounded-full text-center cursor-pointer" key={index}>&#8230;</li>;
+    const onNext = () => {
+        if (currentPage !== lastPage) {
+            onPageChange(currentPage + 1);
         }
-        return (
-          <li key={index}
-            className={`w-6 h-6 text-sm leading-6 rounded-full text-center ${pageNumber === currentPage ? 'bg-blue-400 text-white cursor-not-allowed':'bg-slate-200 cursor-pointer'}`}
-            onClick={() => onPageChange(pageNumber)}
-          >
-            {pageNumber}
-          </li>
-        );
-      })}
-      <li
-        className={`bg-slate-200 w-6 h-6 text-sm leading-6 rounded-full text-center flex justify-center items-center ${currentPage === lastPage ?'opacity-50 cursor-not-allowed':'cursor-pointer'}`}
-        onClick={onNext}
-      >
-        <BsChevronRight/>
-      </li>
-    </ul>
-  );
+
+    };
+
+    const onPrevious = () => {
+        if (currentPage !== 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    let lastPage = paginationRange[paginationRange.length - 1];
+    return (
+        <div className="mt-3 border-t-2 border-dashed pt-2 tracking-wider">
+
+
+            <ul className="flex items-center gap-1">
+                <li
+                    className={`bg-slate-200 w-6 h-6 text-sm leading-6 rounded-full text-center flex justify-center items-center ${currentPage === 1 ? 'opacity-25 cursor-not-allowed' : 'cursor-pointer'}`}
+                    onClick={onPrevious}
+                >
+                    <BsChevronLeft />
+                </li>
+                {paginationRange.map((pageNumber, index) => {
+                    if (pageNumber === DOTS) {
+                        return <li className="bg-slate-200 w-6 h-6 text-sm leading-4 rounded-full text-center" key={index}>...</li>;
+                    }
+                    return (
+                        <li key={index}
+                            className={`w-6 h-6 text-sm leading-6 rounded-full text-center ${pageNumber === currentPage ? 'bg-blue-400 text-white ' : 'bg-slate-200 cursor-pointer'}`}
+                            onClick={() => onPageChange(pageNumber)}
+                        >
+                            {pageNumber}
+                        </li>
+                    );
+                })}
+                <li
+                    className={`bg-slate-200 w-6 h-6 text-sm leading-6 rounded-full text-center flex justify-center items-center ${currentPage === lastPage ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
+                    onClick={onNext}
+                >
+                    <BsChevronRight />
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Pagination;
